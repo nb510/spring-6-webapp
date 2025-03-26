e7b0120ee0caa4e6bcdc82dad7d77ff084531185
nb510 <nikita.burak.dev@gmail.com>
2025-03-26 22:29:57 +0300

    add Publisher

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring6webapp.bootstrap;
 
 import guru.springframework.spring6webapp.domain.Author;
 import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.domain.Publisher;
 import guru.springframework.spring6webapp.repository.AuthorRepository;
 import guru.springframework.spring6webapp.repository.BookRepository;
+import guru.springframework.spring6webapp.repository.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,13 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository,
+                         PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -34,8 +39,17 @@ public class BootstrapData implements CommandLineRunner {
         eric.getBooks().add(ddd);
         authorRepository.save(eric);
 
+        Publisher publisher = new Publisher();
+        publisher.setPublisherName("Alpina Non Fiction");
+        publisher.setCity("Moscow");
+        publisher.setState("Moscow");
+        publisher.setZip("123123");
+
+        publisherRepository.save(publisher);
+
         System.out.println("Inside Bootstrap");
         System.out.printf("Book count: %s%n", bookRepository.count());
         System.out.printf("Author count: %s%n", authorRepository.count());
+        System.out.printf("Publisher count: %s%n", publisherRepository.count());
     }
 }
