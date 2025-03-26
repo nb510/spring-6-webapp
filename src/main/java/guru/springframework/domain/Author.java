4a6afa534322beef1ff1d9e7fbd4666ef07bf19d
nb510 <nikita.burak.dev@gmail.com>
2025-03-26 21:16:34 +0300

    add equals and hashcode to the Book, Author

--- src/main/java/guru/springframework/domain/Author.java
+++ src/main/java/guru/springframework/domain/Author.java
@@ -2,6 +2,7 @@ package guru.springframework.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -37,5 +38,19 @@ public class Author {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
