69d090e97338dc7dd60f1551ac7423c72e040d61
nb510 <nikita.burak.dev@gmail.com>
2025-03-26 22:51:48 +0300

    add Publisher-Book relationship

--- src/main/java/guru/springframework/spring6webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Publisher.java
@@ -1,11 +1,10 @@
 package guru.springframework.spring6webapp.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class Publisher {
@@ -19,6 +18,9 @@ public class Publisher {
     private String city;
     private String state;
     private String zip;
+
+    @OneToMany(mappedBy = "publisher")
+    private Set<Book> books = new HashSet<>();
 
     public Long getId() {
         return id;
@@ -66,6 +68,14 @@ public class Publisher {
 
     public void setZip(String zip) {
         this.zip = zip;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
     }
 
     @Override
